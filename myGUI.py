8ba7b2dc754c23d7f9a7beeaf09e4f3fe0925200
Arthur Chang <arthurc.iphone@gmail.com>
2016-12-13 22:58:01 +0800

    新增註解

--- myGUI.py
+++ myGUI.py
@@ -4,20 +4,23 @@ import tkinter
 
 class ShowResultGUI(threading.Thread):
     def run(self):
-        self.window = tkinter.Tk()
-        self.window.title('NTUB vote')
+        self.window = tkinter.Tk()  # create window
+        self.window.title('ntub installtion art')
+        # set lable info
         self.lable = tkinter.Label(self.window,
                                    text='Hello~',
                                    width=30,
                                    height=15,
                                    font=('Helvetica', 80))
-        self.lable.pack()
+        self.lable.pack()  # put lable in window
+        # set close function
         self.window.protocol('WM_DELETE_WINDOW', self.close)
+        # start window
         self.window.mainloop()
 
     def change_text(self, text):
-        self.lable['text'] = text
-        background_color = False
+        self.lable['text'] = text  # set lable text
+        self.background_color = False
 
     def close(self):
         self.window.quit()
