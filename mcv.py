9a46434fb3c64009550b59046daae72d11c8c9b8
Arthur Chang <arthurc.iphone@gmail.com>
2016-12-15 21:12:12 +0800

    加入 opencv 版 hsv 轉換說明

--- mcv.py
+++ mcv.py
@@ -1,3 +1,26 @@
+"""
+Opencv 的 HSV 與一般的 HSV 有所不同
+
+一般：
+    Ｈ：色相      0~360度
+    Ｓ：飽和度    0~1
+    Ｖ：明度      (黑) 0~1 (白)
+
+轉換公式如下
+
+Opencv 版：
+    Ｈ：0~180 --> H (一般) / 2
+    Ｓ：0~255 --> S (一般) * 255
+    Ｖ：0~255 --> V (一般) * 255
+
+參考：http://vincecc.blogspot.tw/2013/09/opencv-extract-colors-hsv.html
+
+RGB、HSV 轉換器：
+    RGB to HSV (一般)：http://www.rapidtables.com/convert/color/rgb-to-hsv.htm
+    HSV 一般 to RGB：http://www.rapidtables.com/convert/color/hsv-to-rgb.htm
+"""
+
+
 # import
 import cv2
 import imutils
@@ -28,17 +51,24 @@ def init():
 def identification(z, cap, gui, hog):
     # 啟用攝影機
     _, frame = cap.read()
+
     # 讀取攝影機影像
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    # 設定下限（R, G, B）
+
+    # 設定下限（H, S, V）
     lower_red = np.array([175, 150, 0])
-    # 設定上限（R, G, B）
+
+    # 設定上限（H, S, V）
     upper_red = np.array([255, 255, 255])
+
     # 設定遮罩，在遮罩範圍內的顏色為白色其他黑色
     mask = cv2.inRange(hsv, lower_red, upper_red)
+
     # 將 mask 中的白色部份套回原來的顏色，其餘部份維持黑色
     res = cv2.bitwise_and(frame, frame, mask=mask)
+
     count = 0
+
     # 計算有多少點
     for dot in mask:
         if np.any(np.not_equal(dot, z)):
